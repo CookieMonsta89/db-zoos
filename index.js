8fcf2608c3f7a6d3b4399ab453510317cfbc64a9
CookieMonsta89 <jlstan06@gmail.com>
2018-11-13 13:21:39 -0500

    adding data persistence

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const db = require('./data/db.js');
 
 const server = express();
 
@@ -8,7 +9,41 @@ server.use(helmet());
 
 // endpoints here
 
+server.get('/', (req, res) => {
+  res.send('up and running...');
+})
+
+server.get('/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+server.post('/zoos', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ errorMessage: "Please provide a name" });
+  }
+  db.insert({name})
+    .into('zoos')
+    .then(zoo => res.status(201).json({ name }))
+    .catch(err => res.status(400).json({ error: "Error Saving"}))
+    
+})
+
+
+
+
+
+
+
+
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+ console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
